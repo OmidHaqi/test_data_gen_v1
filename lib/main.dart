3c5fa615ad45e6e5db8e41d04d7cb9cb8de8937f
Donya <donya.asayesh@outlook.com>
2024-01-09 00:42:53 +0330

    creatHomeHageScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_data_gen/views/home_page.dart';
 import 'package:test_data_gen/views/my_splash.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MySplash(),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
