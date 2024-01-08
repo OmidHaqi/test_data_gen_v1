b0132f27fc6367074e2fa1eef317fb1005129932
Umut <omid.haqi@outlook.com>
2024-01-09 00:42:12 +0330

    devSplash

--- lib/views/my_splash.dart
+++ lib/views/my_splash.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MySplash extends StatelessWidget {
+class MySplash extends StatefulWidget {
   const MySplash({super.key});
 
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Test data gen app"),
+        child: Column(children: [],),
       ),
     );
   }
