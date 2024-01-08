4473fcbea29c407fec695b56321ba536bfb0c12f
Umut <omid.haqi@outlook.com>
2024-01-09 00:41:55 +0330

    addString

---
+++ lib/views/res/strings.dart
@@ -0,0 +1,10 @@
+class Strings {
+
+  static const String splash = "Test Data Generator App";
+  static const String chooseAlgorithm = "choose Algorithm";
+  static const String enterConditions = "enter conditions";
+  static const String result = "result";
+
+
+  
+}
