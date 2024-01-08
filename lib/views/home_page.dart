a7b8a63c2f0596ada92591ddaf6e0ed7ee695334
Donya <donya.asayesh@outlook.com>
2024-01-09 01:13:36 +0330

    creatFirstRowInHomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:test_data_gen/views/res/strings.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var sizeH = MediaQuery.of(context).size.height;
+    double sizeH = MediaQuery.of(context).size.height;
+    double sizeW = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: [
-            Row(
-              children: [
-                Text("data"),
-                Icon(Icons.info),
-                Icon(Icons.history),
-              ],
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                  sizeW / 19.65, sizeH / 13.3125, sizeW / 19.65, 0),
+              child: Row(
+                children: [
+                  Expanded(child: Text(Strings.chooseAlgorithm)),
+                  Padding(
+                    padding: EdgeInsets.only(right: sizeW / 78.6),
+                    child: Icon(Icons.info_outline),
+                  ),
+                  Icon(Icons.history),
+                ],
+              ),
             )
           ],
         ),
