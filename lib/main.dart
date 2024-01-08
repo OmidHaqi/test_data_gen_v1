c03e08c99d3543d74c4c885f072b746ef55a65b0
Umut <omid.haqi@outlook.com>
2024-01-09 01:22:01 +0330

    createSplash

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MySplash(),
     );
   }
 }
--- lib/views/my_splash.dart
+++ lib/views/my_splash.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:test_data_gen/views/home_page.dart';
+import 'package:test_data_gen/views/res/paths.dart';
+import 'package:test_data_gen/views/res/strings.dart';
 
 class MySplash extends StatefulWidget {
   const MySplash({super.key});
@@ -9,10 +15,45 @@ class MySplash extends StatefulWidget {
 
 class _MySplashState extends State<MySplash> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(children: [],),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 200),
+        child: Center(
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(Paths.imgSplash,height: 330,width: 330,),
+                  Text(Strings.splash)
+                ],
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: SpinKitRing(
+                  size: 45,
+                  color: Color.fromARGB(255, 68, 164, 126),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
