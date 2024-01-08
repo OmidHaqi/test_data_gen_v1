3cdd40e4c9df15a6ce436713f6862a095b9d72ed
Umut <omid.haqi@outlook.com>
2024-01-09 00:42:19 +0330

    addPath

---
+++ lib/views/res/paths.dart
@@ -0,0 +1,7 @@
+class Paths {
+  static const String imgSplash = "assets/images/img_splash.png";
+  static const String imgDNAresult = "assets/images/img_dna_result.png";
+  static const String imgDNAalgorithm = "assets/images/img_dna_algorithm.jpeg";
+  static const String imgGrayWolfAlgorithm = "assets/images/img_gray_wolf_algorithm.jpeg";
+  static const String imgGrayWolfResult = "assets/images/img_gray_wolf_result.png";
+}
