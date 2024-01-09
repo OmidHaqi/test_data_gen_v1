ae688c457a94f894dc35e024a07b88bd048f24fd
Donya <donya.asayesh@outlook.com>
2024-01-09 18:46:09 +0330

    creatinputConditionPage

--- lib/views/inputCondition.dart
+++ lib/views/inputCondition.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:test_data_gen/views/res/strings.dart';
 
-class inputCondition extends StatefulWidget {
+class InputCondition extends StatefulWidget {
   @override
-  State<inputCondition> createState() => _inputConditionState();
+  State<InputCondition> createState() => _InputConditionState();
 }
 
-class _inputConditionState extends State<inputCondition> {
-  List filds = [];
+class _InputConditionState extends State<InputCondition> {
+  List filds = [TextFormField(
+    decoration: const InputDecoration(
+      labelStyle: TextStyle(color: Color.fromARGB(255, 68, 164, 126)),
+      labelText: 'conditions',
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 68, 164, 126),width: 2),
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(width: 2,color: Color.fromARGB(255, 207, 233, 217))
+    ),)
+  ),TextFormField(
+    decoration: const InputDecoration(
+      labelStyle: TextStyle(color: Color.fromARGB(255, 68, 164, 126)),
+      labelText: 'conditions',
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 68, 164, 126),width: 2),
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(width: 2,color: Color.fromARGB(255, 207, 233, 217))
+    ),)
+  ),];
   @override
   Widget build(BuildContext context) {
     double sizeH = MediaQuery.of(context).size.height;
@@ -15,68 +35,118 @@ class _inputConditionState extends State<inputCondition> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 204, 238, 221),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                  sizeW / 19.65, sizeH / 8.114285714, sizeW / 19.65, 0),
-              child: Container(
-                height: sizeH / 6.042553191,
-                width: sizeW / 1.113314448,
-                color: Colors.amber,
-                child: ListView(),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(sizeW / 19.65, sizeH / 35.5, 0, 0),
-                child: TextButton(
-                  onPressed: () {},
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding:  EdgeInsets.fromLTRB(sizeW/19.65, sizeH/13.3125, 0, 0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
                   child: Text(
-                    "Solve",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  style: ButtonStyle(
-                      elevation: MaterialStateProperty.all(5),
-                      shadowColor: MaterialStateProperty.all(Colors.black),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(9)),
+                        'input',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                      backgroundColor: MaterialStatePropertyAll(
-                          Color.fromARGB(255, 95, 192, 146))),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                  sizeW / 13.1, sizeH / 23.666666667, sizeW / 13.1, 0),
-              child: SizedBox(
-                width: sizeW / 1.18373494,
-                height: sizeH / 4,
-                child: Column(
-                  children: [
-                    Align(alignment: Alignment.centerLeft,
-                        child: Text(
-                      "genetic algorithm",
-                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),
-                    )),
-                    SizedBox(
-                      height: sizeH / 56.8,
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    sizeW / 19.65, sizeH /25.058823529, sizeW / 19.65, 0),
+                child: Container(
+                  padding: EdgeInsets.only(top: 10),
+                  height: sizeH / 3,
+                  width: sizeW / 1.113314448,
+                  child: ListView(
+                    children: filds
+                        .map((textFormField) => Container(
+                              padding: EdgeInsets.only(bottom: 10, top: 5),
+                              child: textFormField,
+                            ))
+                        .toList(),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding:
+                      EdgeInsets.fromLTRB(sizeW / 19.65, sizeH / 35.5, 0, 0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Solve",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
                     ),
-                    Text(
-                        "Genetic algorithm is a computational technique that simulates the process of natural selection to solve complex problems. It has been widely applied in various fields, including software engineering. One of its applications in software engineering is test data generation.",style: TextStyle(fontWeight: FontWeight.w400,height: 1.75),),
-                  ],
+                    style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(5),
+                        shadowColor: MaterialStateProperty.all(Colors.black),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(9)),
+                        ),
+                        backgroundColor: MaterialStatePropertyAll(
+                            Color.fromARGB(255, 95, 192, 146))),
+                  ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    sizeW / 13.1, sizeH / 23.666666667, sizeW / 13.1, 0),
+                child: SizedBox(
+                  width: sizeW / 1.18373494,
+                  height: sizeH / 4,
+                  child: Column(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "genetic algorithm",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
+                          )),
+                      SizedBox(
+                        height: sizeH / 56.8,
+                      ),
+                      Text(
+                        "Genetic algorithm is a computational technique that simulates the process of natural selection to solve complex problems. It has been widely applied in various fields, including software engineering. One of its applications in software engineering is test data generation.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400, height: 1.75),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              filds.add(field);
+            });
+          },
+          backgroundColor: Color.fromARGB(255, 68, 164, 126),
+          tooltip: 'Add Text Field',
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
         ),
       ),
     );
   }
+
+  TextFormField field = TextFormField(
+    decoration: const InputDecoration(
+      labelStyle: TextStyle(color: Color.fromARGB(255, 68, 164, 126)),
+      labelText: 'conditions',
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromARGB(255, 68, 164, 126),width: 2),
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(width: 2,color: Color.fromARGB(255, 207, 233, 217))
+    ),)
+  );
 }
