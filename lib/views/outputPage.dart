8dad052067c37f42ebffce21c808754dec603ce3
Donya <donya.asayesh@outlook.com>
2024-01-10 17:15:31 +0330

    Third page design

--- lib/views/outputPage.dart
+++ lib/views/outputPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_data_gen/views/res/paths.dart';
 
 class OutPutPage extends StatefulWidget {
   @override
@@ -21,6 +22,137 @@ class _OutPutPageState extends State<OutPutPage> {
               child: Text(
                 'result',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            width: sizeW / 0.9825,
+            height: sizeH / 2.13,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                image: DecorationImage(
+                    image: AssetImage(
+                      Paths.imgDNAresult,
+                    ),
+                    fit: BoxFit.cover)),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  sizeW / 6.894736842, sizeH / 18.52173913, 0, 0),
+              child: Container(
+                width: sizeW / 2.358974,
+                height: sizeH / 3.8,
+                color: Colors.greenAccent,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: sizeH / 85.2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "X:",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "319",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: sizeH / 85.2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Y:",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "4",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: sizeH / 85.2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Z:",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "4",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: sizeH / 85.2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "ANAR:",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "-103",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: sizeH / 85.2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "TIME:",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "5.36",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("s",style: TextStyle(fontSize: 23,fontWeight: FontWeight.w600)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
