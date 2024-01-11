c741a2be4607a0730f0aac1ed56dc3a393f9e895
Donya <donya.asayesh@outlook.com>
2024-01-11 18:06:31 +0330

    info page design

--- lib/views/res/paths.dart
+++ lib/views/res/paths.dart
@@ -4,4 +4,5 @@ class Paths {
   static const String imgDNAalgorithm = "assets/images/img_dna_algorithm.jpeg";
   static const String imgGrayWolfAlgorithm = "assets/images/img_gray_wolf_algorithm.jpeg";
   static const String imgGrayWolfResult = "assets/images/img_gray_wolf_result.png";
+  static const String img_logo = "assets/images/img_logo.png";
 }
